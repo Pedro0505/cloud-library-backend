defd0c2f04064987aa2795b6a459805fcb186198
pedro <pedrohrds05@hotmail.com>
2022-11-09 18:43:50 -0300

    test(integration): add test to POST /book when have errors

--- __tests__/integration/book.test.ts
+++ __tests__/integration/book.test.ts
@@ -54,11 +54,20 @@ describe('Testing the route /books', () => {
       await prisma.$disconnect();
     });
 
-    it('Testing the response of POST in /books', async () => {
+    it('Testing the sucess response of POST in /books', async () => {
       const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ title: 'Jonh Doe', publicationDate: '2000-01-01T00:00:00.000Z', caption: 'Life of Jonh Doe', writerId: 1 });
 
       expect(status).toBe(201);
       expect(body).toStrictEqual({ data: { id: 6, title: "Jonh Doe", publicationDate: "2000-01-01T00:00:00.000Z", caption: "Life of Jonh Doe" } });
+    });
+
+    describe('Testing when has send a "writerId" that does not exist', () => {
+      it('Testing the error response of POST in /books', async () => {
+        const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ title: 'Jonh Doe', publicationDate: '2000-01-01T00:00:00.000Z', caption: 'Life of Jonh Doe', writerId: 3421 });
+  
+        expect(status).toBe(404);
+        expect(body).toStrictEqual({ error: { message: "Writer Not Found!" }});
+      });
     });
 
     describe('Testing when have a invalid body', () => {
@@ -91,12 +100,81 @@ describe('Testing the route /books', () => {
           expect(body).toStrictEqual({ error: { message: "\"caption\" can't be more than 45" } });
         });
 
-        it('Testing when the "caption" is not a number', async () => {
+        it('Testing when the "caption" is not a string', async () => {
           const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ title: 'Jonh Doe', publicationDate: '2000-01-01T00:00:00.000Z', writerId: 1, caption: 1 });
 
           expect(status).toBe(400);
           expect(body).toStrictEqual({ error: { message: "\"caption\" must be a string" } });
         });
+      });
+
+      describe('Testing the field "publicationDate"', () => {
+        it('Testing when the "publicationDate" is not send', async () => {
+          const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ title: 'Jonh Doe', writerId: 1, caption: 'Life of Jonh Doe' });
+
+          expect(status).toBe(400);
+          expect(body).toStrictEqual({ error: { message: "\"publicationDate\" is required" } });
+        });
+
+        it('Testing when the "publicationDate" is invalid format', async () => {
+          const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ title: 'Jonh Doe', writerId: 1, caption: 'Life of Jonh Doe', publicationDate: '20-08-01T00:00:00.000Z' });
+  
+          expect(status).toBe(400);
+          expect(body).toStrictEqual({ error: { message: "\"publicationDate\" is invalid date format" } });
+        });
+
+        it('Testing when the "publicationDate" is not a string', async () => {
+          const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ title: 'Jonh Doe', writerId: 1, caption: 'Life of Jonh Doe', publicationDate: 1 });
+  
+          expect(status).toBe(400);
+          expect(body).toStrictEqual({ error: { message: "\"publicationDate\" must be a string" } });
+        }); 
+      });
+
+      describe('Testing the field "title"', () => {
+        it('Testing when the "title" is not send', async () => {
+          const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ caption: 'Life of Jonh Doe', publicationDate: '2000-01-01T00:00:00.000Z', writerId: 1 });
+
+          expect(status).toBe(400);
+          expect(body).toStrictEqual({ error: { message: "\"title\" is required" } });
+        });
+
+        it('Testing when the "title" is empty', async () => {
+          const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ title: '', publicationDate: '2000-01-01T00:00:00.000Z', writerId: 1, caption: 'Life of Jonh Doe' });
+
+          expect(status).toBe(400);
+          expect(body).toStrictEqual({ error: { message: "\"title\" can't be empty" } });
+        });
+
+        it('Testing when the "title" is send with more than 45 of length', async () => {
+          const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ title: 's'.repeat(46), publicationDate: '2000-01-01T00:00:00.000Z', writerId: 1, caption: 'Life of Jonh Doe' });
+
+          expect(status).toBe(400);
+          expect(body).toStrictEqual({ error: { message: "\"title\" can't be more than 45" } });
+        });
+
+        it('Testing when the "title" is not a string', async () => {
+          const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ title: 1, publicationDate: '2000-01-01T00:00:00.000Z', writerId: 1, caption: 'Life of Jonh Doe' });
+
+          expect(status).toBe(400);
+          expect(body).toStrictEqual({ error: { message: "\"title\" must be a string" } });
+        });
+      });
+
+      describe('Testing the field "writerId"', () => {
+        it('Testing when the "writerId" is not send', async () => {
+          const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ title: 'Jonh Doe', caption: 'Life of Jonh Doe', publicationDate: '2000-01-01T00:00:00.000Z' });
+
+          expect(status).toBe(400);
+          expect(body).toStrictEqual({ error: { message: "\"writerId\" is required" } });
+        });
+
+        it('Testing when the "writerId" is not a number', async () => {
+          const { body, status } = await request(app).post(PathRoutes.BOOKS).send({ title: 'Jonh Doe', writerId: 'd', caption: 'Life of Jonh Doe', publicationDate: '2000-01-01T00:00:00.000Z' });
+  
+          expect(status).toBe(400);
+          expect(body).toStrictEqual({ error: { message: "\"writerId\" must be a number" } });
+        }); 
       });
     });
   });
