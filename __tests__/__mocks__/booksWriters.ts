8249f642327392e1c272c5e12390833d99c2da80
pedro <pedrohrds05@hotmail.com>
2022-11-09 15:59:36 -0300

    test(integration): add mock to integration test

---
+++ __tests__/__mocks__/booksWriters.ts
@@ -0,0 +1,24 @@
+const booksWritersData = [
+  {
+    booksId: 1,
+    writersId: 1,
+  },
+  {
+    booksId: 2,
+    writersId: 2,
+  },
+  {
+    booksId: 3,
+    writersId: 3,
+  },
+  {
+    booksId: 4,
+    writersId: 1,
+  },
+  {
+    booksId: 5,
+    writersId: 4,
+  }
+];
+
+export default booksWritersData;
