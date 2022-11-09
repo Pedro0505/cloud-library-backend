a946331f0c0f3be9cc4c19d057db2145a198c31f
pedro <pedrohrds05@hotmail.com>
2022-11-09 17:07:16 -0300

    test(integration): adding seeds to tests

---
+++ __tests__/seeds/books.ts
@@ -0,0 +1,34 @@
+const booksSeed = [
+  {
+    id: 1,
+    title: 'Clean Code',
+    caption: 'A Handbook of Agile Software Craftmanship',
+    publicationDate: new Date('08/01/2008'),
+  },
+  {
+    id: 2,
+    title: 'Refactoring',
+    caption: 'Improving the Design of Existing Code',
+    publicationDate: new Date('01/01/1999'),
+  },
+  {
+    id: 3,
+    title: 'Domain Driven Design',
+    caption: 'Tackling Complexity in the Heart of Software',
+    publicationDate: new Date('08/22/2003'),
+  },
+  {
+    id: 4,
+    title: 'Clean Architecture',
+    caption: 'A Craftsman\'s Guide to Software Structure',
+    publicationDate: new Date('09/10/2017'),
+  },
+  {
+    id: 5,
+    title: 'The Pragmatic Programmer',
+    caption: 'Your Journey to Mastery',
+    publicationDate: new Date('09/13/2019'),
+  },
+];
+
+export default booksSeed;
---
+++ __tests__/seeds/writers.ts
@@ -0,0 +1,28 @@
+const writersSeed = [
+  {
+    id: 1,
+    name: 'Robert Cecil Martin',
+    sex: 'Male',
+    birthDate: new Date('05/12/1952'),
+  },
+  {
+    id: 2,
+    name: 'Martin Fowler',
+    sex: 'Male',
+    birthDate: new Date('12/18/1963'),
+  },
+  {
+    id: 3,
+    name: 'Eric Evans',
+    sex: 'Male',
+    birthDate: new Date('07/02/1975'),
+  },
+  {
+    id: 4,
+    name: 'Andy Hunt',
+    sex: 'Male',
+    birthDate: new Date('12/17/1964'),
+  },
+];
+
+export default writersSeed;
